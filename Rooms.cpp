331ff702dbedd8ed5e3720a609af69b84276964b
tsvtkvvv <125773942+tsvtkvvv@users.noreply.github.com>
2024-06-03 16:09:32 +0300

    changes on files

--- Rooms.cpp
+++ Rooms.cpp
@@ -1,25 +1,41 @@
 #include "Rooms.h"
-
+#include <iostream>
+#include <exception>
+#include <string>
 
 
 using namespace std;
 
-class Rooms{
-public:
-	Rooms(double lenght, double wigdt, double height, double usefulArea, double allArea);
 
-double getHeight() const
-{
-	return r_height;
-}
+	Rooms::Rooms(double lenght, double wigdt, double height, double usefulAreaFloor, double allArea, double usefulAreaWall)
+	{
+		if (lenght <= 0 || wigdt <= 0 || height <= 0) {
+			throw std::invalid_argument("Can`t create a room with this parameteres. Enter the parameters high than zero.");
+		}
+		if (usefulAreaFloor <= 0 || allArea <= 0 || usefulAreaWall <= 0) {
+			throw std::invalid_argument("Can`t create a room with this parameteres of area");
+		}
+	}
+	double Rooms::usableWallArea() const {
+		 double usableArea = r_lenght * r_height * 2 + r_wigdt * r_height * 2;
+		 return usableArea;
+	}
+	double Rooms::usableFloorArea() const {
+		return r_wigdt * r_lenght;
+	}
 
-double getUsefulWallArea() const
-{
-	return r_usefulArea;
-}
+	void Rooms::setUsableWallArea(double newArea) {
+		r_usefulAreaWall = newArea;
+	}
 
-string Info() const;
+	void Rooms::setUsableFloorArea(double newArea) {
+		r_usefulAreaFloor = newArea;
+	}
 
-private:
-	double r_lenght, r_wigdt, r_height, r_usefulArea, r_allArea;
-};
+	string Rooms::Info() const {
+		std::stringstream ss
+		ss << "Room (" << r_wigdt << '*' << r_lenght << '*'
+		<< r_height << ")." << "\nUsable wall area: " << usableWallArea << " m2." << "\nUsable floor area: "
+		<< usableFloorArea << " m2."; 
+			return ss.str(); 
+	}
--- Rooms.h
+++ Rooms.h
@@ -9,21 +9,22 @@ class Rooms
 {
 public:
 	Rooms(double lenght, double wigdt, double height, double usefulAreaFloor, double allArea, double usefulAreaWall);
-	double getHeight() const {
-		return r_lenght;
-	}
-	double getUsefulWallArea() const;
+    double usableWallArea() const;
+    double usableFloorArea() const;
 
-	double getUsableWallArea(double newArea) {
-		return r_usefulAreaWall;
-	}
-	double getUsableFloorArea(double newArea) {
-		return r_usefulAreaFloor;
-	}
-	void setUsableWallArea(double newArea);
-	void setUsableFloorArea(double newArea);
+    Rooms(double r_lenght, double r_wigdt, double r_height, double usefulAreaFloor, double allArea, double usefulAreaWall) {
+        this->r_height = r_height;
+        this->r_lenght = r_lenght;
+        this->r_wigdt = r_wigdt;
+        this->r_usefulAreaFloor = usefulAreaFloor;
+        this->r_usefulAreaWall = usefulAreaWall;
+        this->r_allArea = allArea;
+    }
 
-	string Info() const;
+    void setUsableWallArea(double newArea);
+    void setUsableFloorArea(double newArea);
+
+    std::string Info() const;
 
 private:
 	double r_lenght, r_wigdt, r_height, r_usefulAreaWall, r_allArea, r_usefulAreaFloor;
